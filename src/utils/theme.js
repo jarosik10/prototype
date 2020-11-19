59e7d3b1745359e1ee55515377f3cb792ec6c9ea
jarosik10 <bartosz.jaros96@gmail.com>
2020-11-19 13:55:06 +0100

    Add theme utils

---
+++ src/utils/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+    colors: {
+        blue: '#0466C8',
+        darkGray: '#5C677D',
+        lightGray: '#979DAC',
+        black: '#000000',
+        red: '#E51A1A',
+    },
+    zindex: {
+        level1: '1000',
+        level2: '2000',
+        level3: '3000',
+        level4: '4000',
+        level5: '5000',
+        level6: '6000',
+        level7: '7000',
+        level8: '8000',
+        level9: '9000',
+        minus: '-1',
+      }
+}
