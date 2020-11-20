04b2067b6b9a4e2c1c3c7b1e74f375c532dcd30f
jarosik10 <bartosz.jaros96@gmail.com>
2020-11-20 21:43:11 +0100

    Add debounced onresize event handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Context } from './Store';
+import debounce from 'lodash.debounce';
 
 import Layout from './components/Layout/Layout';
 import Header from './components/Header/Header';
@@ -9,6 +10,20 @@ import Login from './components/Login/Login';
 function App() {
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showMobileNavigation, setShowMobileNavigation] = useContext(Context);
+  const [windowDimension, setWindowDimension] = useState(window.innerWidth);
+  const isMobile = windowDimension < 640;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimension(window.innerWidth)
+      console.log(window.innerWidth)
+    }
+
+    const debouncedHandleResize = debounce(handleResize, 50, {'leading': false, 'trailing': true});
+    window.addEventListener('resize', debouncedHandleResize);
+
+    return () => {window.removeEventListener('resize', debouncedHandleResize)};
+  }, []);
   
   const openLogin = () => {
     setShowLoginModal(true);
@@ -24,7 +39,7 @@ function App() {
     <Layout>
       <Header />
       {showLoginModal && <Login closeLogin={closeLogin} />}
-      {showMobileNavigation && <MobileNavigation openLogin={openLogin} />}
+      {isMobile ? showMobileNavigation && <MobileNavigation openLogin={openLogin} /> : <p>asd</p>}
     </Layout>
   );
 }
